7fda5b9a33e0b4f4124112d944570a8b3ec61e40
Kseniia Hanziuk <khanziuk@kse.org.ua>
2024-10-16 13:50:45 +0300

    First three tasks

---
+++ homework2/homework2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  homework2
+//
+//  Created by Ксенія on 16.10.2024.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ homework2/homework2/task3.swift
@@ -0,0 +1,8 @@
+//
+//  task3.swift
+//  homework2
+//
+//  Created by Ксенія on 16.10.2024.
+//
+
+import Foundation
